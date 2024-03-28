bab5101fdfd7b1b7cc666a3f286493abbbe9760c
Michael Botelho <michaelbotelho51@gmail.com>
2024-03-28 12:00:30 -0400

    Added executable and defaulted path to current dir
    Place the script in the desired directory.

--- ExtractIcons.py
+++ ExtractIcons.py
@@ -22,54 +22,54 @@ def PromptWarning(dir_name):
         confirmation = input("Are you sure you want to attempt to extract and clean Icons in the current directory: " + dir_name + " [Y/n]?").lower()
     
     
-def main():
-    # Get specified directory if exists
-    if len(sys.argv) > 1:
-        directory = sys.argv[1] 
-    else:
-        directory = os.path.dirname(os.path.abspath(__file__))
-        # Warning user if directory unspecified
-        PromptWarning(directory)
-        
-   
-    # Get a list of all items (files and folders) in the directory
-    items = os.listdir(directory)
-    
-    
-    for item in items:
-        # Unzip compressed files to root directory
-        if item.endswith('.zip'):  
-            zip_path = os.path.join(directory, item)
-            # Unzip compressed folder into root directory
-            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-                zip_ref.extractall(directory)
-
-        # Extract Icon from ios folder and rename with zip name
-        tmps = os.listdir(directory)
-        for tmp in tmps:
-            
-            # Check if the item is a folder
-            if os.path.isdir(tmp) and not tmp.startswith('.'):
-                tmp_path = os.path.join(directory, tmp)
-                if tmp == 'ios':
-                    icon_name = item.rsplit()[-1].split('.')[0] + '.png'
-                    source_item_path = os.path.join(tmp, 'AppIcon-40@3x.png')
-                    destination_item_path = os.path.join(directory, icon_name)
-                
-                    # Extract the icon into the specified directory
-                    os.rename(source_item_path, destination_item_path)
-
-                # Remove zip and any associated folder (ios, android, web)
-                if os.path.exists(zip_path): os.remove(zip_path)
-                if os.path.exists(tmp_path): shutil.rmtree(tmp_path)
-
-                
+def main():           
     try:
-        pass
+        # Get specified directory if exists
+        if len(sys.argv) > 1:
+            directory = sys.argv[1] 
+        else:
+            #directory = os.path.dirname(os.path.abspath(__file__))
+            directory = os.getcwd()
+            # Warning user if directory unspecified
+            PromptWarning(directory)
+            
+    
+        # Get a list of all items (files and folders) in the directory
+        items = os.listdir(directory)
+        
+        
+        for item in items:
+            # Unzip compressed files to root directory
+            if item.endswith('.zip'):  
+                zip_path = os.path.join(directory, item)
+                # Unzip compressed folder into root directory
+                with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                    zip_ref.extractall(directory)
+
+            # Extract Icon from ios folder and rename with zip name
+            tmps = os.listdir(directory)
+            for tmp in tmps:
+                
+                # Check if the item is a folder (ignore .folders)
+                if os.path.isdir(tmp) and not tmp.startswith('.'):
+                    tmp_path = os.path.join(directory, tmp)
+                    if tmp == 'ios':
+                        icon_name = item.rsplit()[-1].split('.')[0] + '.png'
+                        source_item_path = os.path.join(tmp, 'AppIcon-40@3x.png')
+                        destination_item_path = os.path.join(directory, icon_name)
+                    
+                        # Extract the icon into the specified directory
+                        os.rename(source_item_path, destination_item_path)
+
+                    # Remove zip and any associated folder (ios, android, web)
+                    if os.path.exists(zip_path): os.remove(zip_path)
+                    if os.path.exists(tmp_path): shutil.rmtree(tmp_path)
+                    
+    except KeyboardInterrupt:
+        exit(0)
     except:
         print("ERROR: could not extract icons.")
         
-
     
 if __name__ == '__main__':
     main()
